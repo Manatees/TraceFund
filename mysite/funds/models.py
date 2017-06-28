7380027a20c60adeb841afc515ecf2617dcffadc
Manatees <manatees@live.cn>
2017-06-28 23:49:28 +0800

    add fund history update method

--- mysite/funds/models.py
+++ mysite/funds/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from . import utilities
+import datetime
 
 class Fund(models.Model):
 	fund_name = models.CharField(max_length=200)
@@ -13,11 +14,24 @@ class Fund(models.Model):
 	def latest_fund_data(self):		
 		return self.fundhistory_set.order_by('-date')[0]
 
+	def current_redemption_rate(self, current_days):
+		rates = list(self.redemptionratetable_set.order_by('days'))
+		return Fund.__get_valid_redemption_rate(rates, current_days)
+	
+	def __get_valid_redemption_rate(rates, days):
+		for rate in rates[:-1]:
+			if days < rate.days:
+				return rate.rate_value
+		return rates[-1].rate_value
+
+
 	def __refresh_fund_data(self, in_days):
 		return utilities.do_it(self.fund_code, in_days)
 
 	def update(self):
-		self.latest_remote_fund_data = self.__refresh_fund_data(1)
+		current_date = datetime.datetime.now().date()
+		diff_days = (current_date - self.latest_fund_data().date).days
+		self.latest_remote_fund_data = self.__refresh_fund_data(diff_days)
 
 
 class FundHistory(models.Model):
